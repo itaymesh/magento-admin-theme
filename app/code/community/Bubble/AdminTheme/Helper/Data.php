905009f728be68519794227fc9ed01d9eb4d52b2
itaymesh <itaymesh@gmail.com>
2013-06-29 14:03:44 +0300

    Add RTL LTR switch support using LESS variables.

--- app/code/community/Bubble/AdminTheme/Helper/Data.php
+++ app/code/community/Bubble/AdminTheme/Helper/Data.php
@@ -2,4 +2,13 @@
 
 class Bubble_AdminTheme_Helper_Data extends Mage_Core_Helper_Abstract
 {
+	var $_rtl_locale_codes = array('he_IL', 'ar', 'ur', 'ur_PK', );	
+	
+	public function isRtlLocale() {
+	
+		$_store_locale_code = Mage::app()->getLocale()->getLocaleCode();
+			
+		return in_array($_store_locale_code, $this->_rtl_locale_codes);
+	
+	}
 }
--- app/code/community/Bubble/AdminTheme/Model/Adminhtml/Observer.php
+++ app/code/community/Bubble/AdminTheme/Model/Adminhtml/Observer.php
@@ -9,5 +9,44 @@ class Bubble_AdminTheme_Model_Adminhtml_Observer
         foreach (array('layout', 'template', 'skin', 'locale') as $type) {
             Mage::getDesign()->setTheme($type, $theme);
         }
+        
+        
+        $headBlock = Mage::getLayout()->getBlock('head');
+        
+        $headBlock->addCss('app.css');
+        
+    }
+    
+    public function setLessVariables(Varien_Event_Observer $observer) {
+    	 
+    	$response = $observer->getEvent()->getResponse();
+    	 
+    	if 	(Mage::helper('bubble_admintheme')->isRtlLocale()) {
+    		$response->setLessVariables($this->_getRtlLessVariables());
+    	}
+    	 
+    	else {
+    		$response->setLessVariables($this->_getLtrLessVariables());
+    	}
+    
+    	return $this;
+    }
+    
+    private function _getRtlLessVariables() {
+    	return array(
+    			'bi-app-left'      		 => 'right',
+    			'bi-app-right'     		 => 'left',
+    			'bi-app-direction' 		 => 'rtl',
+    			'bi-app-invert-direction' => 'ltr'
+    	);
+    }
+    
+    private function _getLtrLessVariables() {
+    	return array(
+    			'bi-app-left'      		 => 'left',
+    			'bi-app-right'     		 => 'right',
+    			'bi-app-direction' 		 => 'ltr',
+    			'bi-app-invert-direction' => 'rtl'
+    	);
     }
 }

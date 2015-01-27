97c3a2c1aa1d526e0e19cb76bcef7d1145a1a563
root <itay@studioraz.co.il>
2015-01-27 13:29:34 +0200

    update

---
+++ skin/adminhtml/default/go/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+
+var less      = require('gulp-less');
+var minifycss = require('gulp-minify-css');
+var util    = require('gulp-util');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var notify     = require('gulp-notify');
+var clean      = require('gulp-clean');
+var livereload = require('gulp-livereload');
+var lr         = require('tiny-lr');
+var server     = lr();
+var rename     = require('gulp-rename');
+var cssflip =        require('gulp-css-flip');
+
+gulp.task('less', function() {
+    return gulp.src('less/app.less')
+
+        .pipe(less()).on('error', function(e){
+            util.log(e);
+        })
+        //.pipe(minifycss())
+        .pipe(gulp.dest('dist'))
+        //.pipe(rename({suffix: '-rtl'}))
+        //.pipe(cssflip.gulp())
+        //.pipe(gulp.dest('dist/css'))
+        .pipe(livereload(server))
+        .pipe(notify({
+            message: 'Successfully compiled LESS'
+        }));
+});
+
+// Watch
+gulp.task('watch', function() {
+    // Listen on port 35729
+    server.listen(35729, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        gulp.watch('src/img/**/*', ['img']);
+
+        // Watch .less files
+        gulp.watch('src/less/**/*.less', ['less']);
+
+        // Watch .js files
+        gulp.watch('src/js/**/*.js', ['lint', 'js']);
+    });
+});

fe1cfb60cfa19ed4d82595a5a674ea5d3e811857
root <itay@studioraz.co.il>
2015-01-27 14:29:44 +0200

    add RTL LTR dynamic uplaod

--- app/code/community/Bubble/AdminTheme/Model/Adminhtml/Observer.php
+++ app/code/community/Bubble/AdminTheme/Model/Adminhtml/Observer.php
@@ -1,47 +1,25 @@
 <?php
 
-class Bubble_AdminTheme_Model_Adminhtml_Observer
-{
-    public function setTheme()
-    {
-        $theme = Mage::getStoreConfig('bubble_admintheme/config/theme');
-        Mage::getDesign()->setTheme($theme);
-        foreach (array('layout', 'template', 'skin', 'locale') as $type) {
-            Mage::getDesign()->setTheme($type, $theme);
-        }
-                
-    }
-    
-    public function setLessVariables(Varien_Event_Observer $observer) {
-    	 
-    	$response = $observer->getEvent()->getResponse();
-    	 
-    	if 	(Mage::helper('bubble_admintheme')->isRtlLocale()) {
-    		$response->setLessVariables($this->_getRtlLessVariables());
-    	}
-    	 
-    	else {
-    		$response->setLessVariables($this->_getLtrLessVariables());
-    	}
-    
-    	return $this;
-    }
-    
-    private function _getRtlLessVariables() {
-    	return array(
-    			'bi-app-left'      		 => 'right',
-    			'bi-app-right'     		 => 'left',
-    			'bi-app-direction' 		 => 'rtl',
-    			'bi-app-invert-direction' => 'ltr'
-    	);
-    }
-    
-    private function _getLtrLessVariables() {
-    	return array(
-    			'bi-app-left'      		 => 'left',
-    			'bi-app-right'     		 => 'right',
-    			'bi-app-direction' 		 => 'ltr',
-    			'bi-app-invert-direction' => 'rtl'
-    	);
-    }
+class Bubble_AdminTheme_Model_Adminhtml_Observer {
+	public function setTheme() {
+		$theme = Mage::getStoreConfig( 'bubble_admintheme/config/theme' );
+		Mage::getDesign()->setTheme( $theme );
+		foreach ( array( 'layout', 'template', 'skin', 'locale' ) as $type ) {
+			Mage::getDesign()->setTheme( $type, $theme );
+		}
+
+	}
+
+
+	public function controllerActionLayoutLoadBefore( Varien_Event_Observer $observer ) {
+		$_head = $observer->getEvent()->getLayout()->getBlock( 'head' );
+
+		if ( Mage::helper( 'bubble_admintheme' )->isRtlLocale() ) {
+			$_head->addItem( 'skin_css', 'dist/app-rtl.css' );
+		} else {
+			$_head->addItem( 'skin_css', 'dist/app.css' );
+		}
+
+
+	}
 }
